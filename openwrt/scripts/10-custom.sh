3e3d3e77b0c3c35f6c92bfc961b895a29ce514e8
oppen321 <zj18139624826@gmail.com>
2024-11-07 23:36:52 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -3,8 +3,9 @@
 # 自定义脚本
 
 # 个性化修改
-https://raw.githubusercontent.com/oppen321/Diy/refs/heads/main/bg.webp
-https://raw.githubusercontent.com/oppen321/Diy/refs/heads/main/banner
+sed -i "s/hostname='.*'/hostname='ZeroWrt'/g" package/base-files/files/bin/config_generate
+curl -s https://raw.githubusercontent.com/oppen321/Diy/refs/heads/main/bg.webp -o package/new/luci-theme-argon/luci-theme-argon/htdocsluci-static/argon/img/bg.webp
+curl -s https://raw.githubusercontent.com/oppen321/Diy/refs/heads/main/banner -o package/base-files/files/etc/banner
 
 echo "
 # luci-app-mihomo
