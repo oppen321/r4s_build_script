f3d6cf63092dc6572e7f7dd1e6874472b4b0e028
oppen321 <zj18139624826@gmail.com>
2024-11-07 22:45:50 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 # 自定义脚本
+
+echo "
+# luci-app-mihomo
+CONFIG_PACKAGE_luci-app-mihomo=y
+
+# mihomo
+CONFIG_PACKAGE_mihomo=y
+
+# Openclash
+CONFIG_PACKAGE_luci-app-openclash=y
+
+# AdguardHome
+CONFIG_PACKAGE_luci-app-adguardhome=y
+
+# 易有云
+#CONFIG_PACKAGE_luci-app-linkease=y
+
+" >> .config
